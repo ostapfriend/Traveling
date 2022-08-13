3083934cb53e7d848933cb5d227faadd668db347
Ostap Salo <jlumon211@gmail.com>
2022-08-13 21:01:28 +0300

    update router

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
         <Navbar />
         <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/traveling' element={<Home/>}/>
             <Route path='/services' element={<Services/>}/>
             <Route path='/products' element={<Products/>}/>
             <Route path='/sign-up' element={<SignUp/>}/>
